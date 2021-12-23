60fbad7fd43c841b95b8271a63f869513a52b607
MathiasGretland <mathiasgretland@gmail.com>
2021-12-23 17:19:22 +0100

    New project

---
+++ warmup/app.js
@@ -0,0 +1,32 @@
+/*
+var user = { name: "Dev Mathias" }
+
+//for(let i = 0; i < 10; i++)
+for(var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log(`This is i ${i}`)
+
+
+function sayHello() {
+    var name = 'Mathias';
+    console.log(name)
+}
+
+sayHello();
+*/
+
+//const name = 'Mathias';
+let name = 'Mathias';
+
+name = 'Maria';
+
+console.log(name);
+
+
+const users = ['Mathias', 'Maria', 'Lauren', 'Jimmyboy'];
+
+users.push('Magnus')
+
+console.log(users);
